8ac19fbee36c1018872b2d29843faccc967511a5
mujeebahmad <abdulmujeebahmad03@gmail.com>
2025-08-27 18:27:18 +0100

    fix: improve prisma client initialization for better global access
    - Added global declaration for PrismaClient to allow shared access across modules.
    - Updated Prisma client export to utilize the global instance
      if available, enhancing performance in non-production environments.

--- src/shared/lib/prisma.ts
+++ src/shared/lib/prisma.ts
@@ -2,5 +2,12 @@ import { PrismaNeon } from "@prisma/adapter-neon";
 
 import { PrismaClient } from "@/generated/prisma/client";
 
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
 const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL });
-export const prisma = new PrismaClient({ adapter });
+
+export const prisma = globalThis.prisma ?? new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
